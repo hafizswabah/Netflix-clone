48e5ae59dccd98532c836e4283e901ada062f35d
hafizswabah <swabah.a.avd121@gmail.com>
2023-03-31 12:04:35 +0530

    Netflix Completed UI

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
    <Banner/>
    <Post tittle='Netflix Orginals' url={`discover/tv?api_key=${APIKEY}&with_network=213`}/>
    <Post tittle='Actions' Small url={`discover/movie?api_key=${APIKEY}&with_genres=28`}/>
+   <Post tittle='Comedy' Small url={`discover/movie?api_key=${APIKEY}&with_genres=35`}/>
+   <Post tittle='Horror' Small url={`discover/movie?api_key=${APIKEY}&with_genres=27`}/>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import '../NavBar/NavBar.css'
+import { RiSearchLine } from 'react-icons/ri'
 function NavBar() {
   return (
     <div className='navBar'>
+     
         <img className='logo' src="https://wallpaperaccess.com/full/6075689.png"
          alt="" />
+         <div className="navheadings">
+         <h3 className='navheading'>Homes</h3>
+         <h3 className='navheading'>TV Shows</h3>
+         <h3 className='navheading'>Movies</h3>
+         </div>
+     
+<div className="second-sec">
+  <RiSearchLine className='searchLogo'/>
+  <h2>Kids</h2>
+  <h2>DVD</h2>
+</div>
          <img className='avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
          alt="" />
     </div>
